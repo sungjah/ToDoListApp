5cf80a37347c86ec3dd88442a08298cf284750f3
sungjah <sungjaehong10@gmail.com>
2023-08-15 23:19:11 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by SungJae Hong on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
